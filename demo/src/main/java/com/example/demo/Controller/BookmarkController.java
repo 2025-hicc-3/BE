2f3c3ed219aa275d84e5d6ccc99eaea3ce74649f
진지충 <genejichoong@gmail.com>
2025-07-16 17:42:44 +0900

    기능 업데이트

--- demo/src/main/java/com/example/demo/Controller/BookmarkController.java
+++ demo/src/main/java/com/example/demo/Controller/BookmarkController.java
@@ -7,6 +7,7 @@ import com.example.demo.ResponseDto.BookmarkResponseDto;
 import com.example.demo.Service.BookmarkService;
 import com.example.demo.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,8 +21,22 @@ public class BookmarkController {
 
     private final BookmarkService bookmarkService;
 
-    @GetMapping("/{userId}/bookmarks")
+    @GetMapping("/{userId}/bookmarks") // 아이디 보고 북마크 가져오기
     public List<BookmarkResponseDto> getBookmarksByUser(@PathVariable Long userId) {
         return bookmarkService.getBookmarksByUser(userId);
+    }
+
+    // 북마크 추가
+    @PostMapping("/{postId}")
+    public ResponseEntity<?> bookmark(@PathVariable Long postId, @RequestParam Long userId) {
+        bookmarkService.addBookmark(userId, postId);
+        return ResponseEntity.ok("북마크 완료");
+    }
+
+    // 북마크 취소
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<?> unbookmark(@PathVariable Long postId, @RequestParam Long userId) {
+        bookmarkService.removeBookmark(userId, postId);
+        return ResponseEntity.ok("북마크 취소됨");
     }
 }
